1abf58b593c8da19a6641dc3ef8af164be6d7bca
SouthRiver <52597530+South-River@users.noreply.github.com>
2024-02-28 14:51:45 +0800

    Update swarm_bridge_test_node.cpp

--- src/swarm_bridge_test_node.cpp
+++ src/swarm_bridge_test_node.cpp
@@ -19,25 +19,20 @@ int main(int argc, char **argv)
     ros::Publisher test_pub = nh.advertise<nav_msgs::Odometry>("test", 10);
     swarm_bridge->subscribe<nav_msgs::Odometry>([&](nav_msgs::Odometry msg){test_pub.publish(msg);});
 
-    // add a subscriber with customized topic name, 
-    // it will only subscribe publisher with same topic name.
+    // add a subscriber with a customized topic name, 
+    // it will only subscribe to publishers with same topic name.
     ros::Publisher test2_pub = nh.advertise<visualization_msgs::Marker>("test2", 10);
     swarm_bridge->subscribe<visualization_msgs::Marker>("vis", [&](visualization_msgs::Marker msg){test2_pub.publish(msg);});
 
     ros::Rate r(100);
     while (ros::ok())
     {
-        // publish data using default topic name.
+        // publish data using the default topic name.
         nav_msgs::Odometry msg;
-        msg.header.frame_id = "world";
-        msg.header.stamp = ros::Time::now();
-        msg.child_frame_id = "0";
         swarm_bridge->publish<nav_msgs::Odometry>(msg);
 
-        // publish data using customized topic name.
+        // publish data using customized topic names.
         visualization_msgs::Marker msg2;
-        msg2.header.frame_id = "world";
-        msg2.header.stamp = ros::Time::now();
         swarm_bridge->publish<visualization_msgs::Marker>("vis", msg2);
     
         r.sleep();
